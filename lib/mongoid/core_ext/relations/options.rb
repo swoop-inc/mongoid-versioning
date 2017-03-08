21f09d9d4d0df41f404c8f7eae7d4deb750faf40
Nishant Dalmia <nishant1851@gmail.com>
2017-03-08 18:42:00 +0530

    fix : can't modify frozen array

--- lib/mongoid/core_ext/relations/options.rb
+++ lib/mongoid/core_ext/relations/options.rb
@@ -1,7 +1,9 @@
 module Mongoid
   module Relations
     module Options
-      COMMON << :versioned
+      options = OPTIONS
+      send :remove_const, :OPTIONS		
+      send :const_set, :OPTIONS, options + [:versioned]
     end
   end
 end
