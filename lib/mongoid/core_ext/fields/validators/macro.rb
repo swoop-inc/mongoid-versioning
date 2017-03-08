5e6fd30270c273a4e131c6d250b49073e2274004
Nishant Dalmia <nishant1851@gmail.com>
2017-03-08 18:42:58 +0530

    fix - can't modify frozen array

--- lib/mongoid/core_ext/fields/validators/macro.rb
+++ lib/mongoid/core_ext/fields/validators/macro.rb
@@ -2,7 +2,9 @@ module Mongoid
   module Fields
     module Validators
       module Macro
-        OPTIONS << :versioned
+        options = OPTIONS
+        send :remove_const, :OPTIONS		
+        send :const_set, :OPTIONS, options + [:versioned]
       end
     end
   end
